a27e62388b9b3a5cfed295ab309a819724b9a44f
Apple <tiffine.koch@gmail.com>
2016-01-28 11:57:31 -0800

    bower stuff

--- models/clogs.js
+++ models/clogs.js
@@ -1,23 +1,8 @@
-"use strict";
+'use strict';
 
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-module.exports = mongoose.model('..models/clog');
-
-router.get('/', function(req, res, next) {
-  Clog.find({}, function(err, clogs) {
-    if (err) return res.status(400).send(err);
-    // res.send(items);
-    res.send(clogs);
-  });
+module.exports = mongoose.model('Clog', {
+  price: Number,
+  description: String
 });
-
-router.post('/', function(req, res, next) {
-  Clog.create({}, function(err, clogs) {
-    if (err) return res.status(400).send(err);
-    // res.send(items);
-    res.send();
-  });
-});
-
-module.exports = router;
--- routes/clogs.js
+++ routes/clogs.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var Clog = require('../models/clog');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Tiff's App" });
+  Clog.find({}, function(err, clogs) {
+    res.send(clogs);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  Clog.create(req.body, function(err) {
+    res.send();
+  });
 });
 
 module.exports = router;
