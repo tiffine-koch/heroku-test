cd4ed2e4f76cc963251e6518a4d364bb4dcfab15
Apple <tiffine.koch@gmail.com>
2016-01-28 12:05:51 -0800

    update

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/', function(req, res, next) {
-  Clog.find({}, function(err, clogs) {
-    if (err) return res.status(400).send(err);
-    // res.send(items);
-    res.send(clogs);
-  });
-});
-
-router.post('/', function(req, res, next) {
-  Clog.create({}, function(err, clogs) {
-    if (err) return res.status(400).send(err);
-    // res.send(items);
-    res.send();
-  });
-});
-
 module.exports = router;
